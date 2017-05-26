6d5c18b237943dc0f4d1c2ee6885e381ac22a832
Piotr Szul <piotr.szul@csiro.au>
2017-05-26 17:08:53 +1000

    Added actual reqquerying of the resources

---
+++ src/main/java/au/gov/dhs/bom/RunTheSouceApp.java
@@ -0,0 +1,59 @@
+package au.gov.dhs.bom;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.flume.Channel;
+import org.apache.flume.ChannelSelector;
+import org.apache.flume.Context;
+import org.apache.flume.Event;
+import org.apache.flume.Transaction;
+import org.apache.flume.channel.ChannelProcessor;
+import org.apache.flume.channel.MemoryChannel;
+import org.apache.flume.channel.ReplicatingChannelSelector;
+import org.apache.flume.conf.Configurables;
+
+import au.gov.dhs.bom.flume.BomObservationSource;
+import au.gov.dhs.bom.flume.BomObservationSourceConstants;
+
+public class RunTheSouceApp {
+
+	public static void main(String[] args) throws Exception {
+		BomObservationSource source;
+		MemoryChannel channel;
+		
+	    source = new BomObservationSource();
+	    channel = new MemoryChannel();
+	    Configurables.configure(channel, new Context());
+
+	    List<Channel> channels = new ArrayList<Channel>();
+	    channels.add(channel);
+
+	    ChannelSelector rcs = new ReplicatingChannelSelector();
+	    rcs.setChannels(channels);
+
+	    source.setChannelProcessor(new ChannelProcessor(rcs));		
+		
+		
+		Context context = new Context();
+		Path relativeIDT60920Path = Paths.get("src/test/xml/IDT60920.xml");
+		context.put(BomObservationSourceConstants.RESOURCE_URL,
+				relativeIDT60920Path.toAbsolutePath().toUri().toASCIIString());
+		context.put(BomObservationSourceConstants.POOLING_INTERVAL_IN_SEC, String.valueOf(5));
+		Configurables.configure(source, context);
+		source.start();
+
+		while (true) {
+			Transaction txn = channel.getTransaction();
+			txn.begin();
+			Event e = channel.take();
+			System.out.println(e);
+			txn.commit();
+			txn.close();
+		}
+		//source.stop();
+	}
+
+}
--- src/main/java/au/gov/dhs/bom/flume/BomObservationSource.java
+++ src/main/java/au/gov/dhs/bom/flume/BomObservationSource.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,67 +28,62 @@ import org.slf4j.LoggerFactory;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Throwables;
 
-public class BomObservationSource extends AbstractSource
-	implements EventDrivenSource, Configurable {
+public class BomObservationSource extends AbstractSource implements EventDrivenSource, Configurable {
 
 	private static final Logger logger = LoggerFactory.getLogger(BomObservationSource.class);
-	
+
 	private SourceCounter sourceCounter;
 	private ScheduledExecutorService executor;
-	private EventReader reader;
-	
+	//private EventReader reader;
+
 	// // configurable
 	private int batchSize = 100;
 	private URL resourceURL;
 	private long poolingIntervalInSec = 60;
-	
-	
+
 	@Override
 	public synchronized void configure(Context context) {
-		
+
 		try {
 			resourceURL = new URL(context.getString(BomObservationSourceConstants.RESOURCE_URL));
 		} catch (MalformedURLException ex) {
 			logger.error("resourceUrl is not a valid url", ex);
 			Throwables.propagate(ex);
 		}
-		
-		poolingIntervalInSec = context.getInteger("poolingIntervalInSec" , 60);
-		
+
+		poolingIntervalInSec = context.getInteger(BomObservationSourceConstants.POOLING_INTERVAL_IN_SEC, 
+				BomObservationSourceConstants.DEF_POOLING_INTERVAL_IN_SEC);
+
 		if (sourceCounter == null) {
 			sourceCounter = new SourceCounter(getName());
 		}
-	
-		if (reader == null) {
-			reader = new URLEventReader(resourceURL);
-		}
+
 	}
 
 	@Override
 	public synchronized void start() {
 		// TODO Auto-generated method stub
-		
+
 		executor = Executors.newSingleThreadScheduledExecutor();
-		
-	    Runnable runner = new BomServiceRunable(sourceCounter);
-	    executor.scheduleWithFixedDelay(
-	        runner, 0, poolingIntervalInSec, TimeUnit.SECONDS);
-		
+
+		Runnable runner = new BomServiceRunable(sourceCounter);
+		executor.scheduleWithFixedDelay(runner, 0, poolingIntervalInSec, TimeUnit.SECONDS);
+
 		super.start();
 		sourceCounter.start();
 	}
 
 	@Override
 	public synchronized void stop() {
-	    executor.shutdown();
-	    try {
-	      executor.awaitTermination(10L, TimeUnit.SECONDS);
-	    } catch (InterruptedException ex) {
-	    	logger.info("Interrupted while awaiting termination", ex);
-	    }
-	    executor.shutdownNow();
-	    super.stop();
-	    sourceCounter.stop();
+		executor.shutdown();
+		try {
+			executor.awaitTermination(10L, TimeUnit.SECONDS);
+		} catch (InterruptedException ex) {
+			logger.info("Interrupted while awaiting termination", ex);
+		}
+		executor.shutdownNow();
+		super.stop();
+		sourceCounter.stop();
 	}
 
 	@VisibleForTesting
@@ -95,59 +91,66 @@ public class BomObservationSource extends AbstractSource
 		// TODO Auto-generated method stub
 		return sourceCounter;
 	}
-	
-	
-	class BomServiceRunable implements Runnable {
-		
-		private final SourceCounter sourceCounter;
-		
-		
 
-	    public BomServiceRunable(SourceCounter sourceCounter) {
+	class BomServiceRunable implements Runnable {
+
+		private final SourceCounter sourceCounter;
+
+		public BomServiceRunable(SourceCounter sourceCounter) {
 			super();
 			this.sourceCounter = sourceCounter;
 		}
 
 		@Override
-	    public void run() {
-	      int backoffInterval = 250;
-	      try {
-	        while (!Thread.interrupted()) {
-	          List<Event> events = reader.readEvents(batchSize);
-	          if (events.isEmpty()) {
-	            break;
-	          }
-	          sourceCounter.addToEventReceivedCount(events.size());
-	          sourceCounter.incrementAppendBatchReceivedCount();
+		public void run() {
 
-	          try {
-	            getChannelProcessor().processEventBatch(events);
-	          } catch (ChannelFullException ex) {
-	            logger.warn("The channel is full, and cannot write data now. The " +
-	                "source will try again after " + backoffInterval +
-	                " milliseconds");
-	            //hitChannelFullException = true;
-	            //backoffInterval = waitAndGetNewBackoffInterval(backoffInterval);
-	            continue;
-	          } catch (ChannelException ex) {
-	            logger.warn("The channel threw an exception, and cannot write data now. The " +
-	                "source will try again after " + backoffInterval +
-	                " milliseconds");
-	            //hitChannelException = true;
-	            //backoffInterval = waitAndGetNewBackoffInterval(backoffInterval);
-	            continue;
-	          }
-	          backoffInterval = 250;
-	          sourceCounter.addToEventAcceptedCount(events.size());
-	          sourceCounter.incrementAppendBatchAcceptedCount();
-	        }
-	      } catch (Throwable t) {
-	        logger.error("FATAL: " + BomObservationSource.this.toString() + ": " +
-	            "Uncaught exception in SpoolDirectorySource thread. " +
-	            "Restart or reconfigure Flume to continue processing.", t);
-	        //hasFatalError = true;
-	        Throwables.propagate(t);
-	      }
-	    }
-	}	
+			
+			logger.info("Pooling the resource at: " + new Date());
+			System.out.println("Pooling the resource at: " + new Date());
+			
+			EventReader reader = new URLEventReader(resourceURL);
+			int backoffInterval = 250;
+			try {
+				while (!Thread.interrupted()) {
+					List<Event> events = reader.readEvents(batchSize);
+					logger.info("Got batch of size: " + events.size());
+					if (events.isEmpty()) {
+						reader.close();
+						break;
+					}
+					sourceCounter.addToEventReceivedCount(events.size());
+					sourceCounter.incrementAppendBatchReceivedCount();
+
+					try {
+						getChannelProcessor().processEventBatch(events);
+					} catch (ChannelFullException ex) {
+						logger.warn("The channel is full, and cannot write data now. The "
+								+ "source will try again after " + backoffInterval + " milliseconds");
+						// hitChannelFullException = true;
+						// backoffInterval =
+						// waitAndGetNewBackoffInterval(backoffInterval);
+						continue;
+					} catch (ChannelException ex) {
+						logger.warn("The channel threw an exception, and cannot write data now. The "
+								+ "source will try again after " + backoffInterval + " milliseconds");
+						// hitChannelException = true;
+						// backoffInterval =
+						// waitAndGetNewBackoffInterval(backoffInterval);
+						continue;
+					}
+					backoffInterval = 250;
+					sourceCounter.addToEventAcceptedCount(events.size());
+					sourceCounter.incrementAppendBatchAcceptedCount();
+				}
+			} catch (Throwable t) {
+				logger.error("FATAL: " + BomObservationSource.this.toString() + ": "
+						+ "Uncaught exception in SpoolDirectorySource thread. "
+						+ "Restart or reconfigure Flume to continue processing.", t);
+				// hasFatalError = true;
+				Throwables.propagate(t);
+			}
+			System.out.println("Exiting pool at: " + new Date());
+			logger.info("Exiting pool at: " + new Date());
+		}		
+	}
 }
