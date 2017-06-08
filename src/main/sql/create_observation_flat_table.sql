2a621c14735e691e88b578a114e09d82362ea1af
Piotr Szul <piotr.szul@csiro.au>
2017-06-08 13:57:38 +1000

    Cleanup morhpline

---
+++ src/main/sql/create_observation_flat_table.sql
@@ -0,0 +1,51 @@
+DROP TABLE IF EXISTS  observation_flat;
+CREATE TABLE observation_flat(
+    issue_time_utc STRING,
+    issue_time_local STRING,
+-- from station        
+        bom_id:STRING,
+        description:STRING,
+        forecast_district_id:STRING,
+        lat:STRING,
+        lon:STRING,
+        stn_name:STRING,
+        tz:STRING,
+        wmo_id:STRING,
+-- from period
+        time_utc:STRING,
+        time_local:STRING>,
+-- from elements        
+        air_temperature:STRING,
+        apparent_temp:STRING,
+        cloud:STRING,
+        cloud_base_m:STRING,
+        cloud_oktas:STRING,
+        cloud_type_id:STRING,
+        delta_t:STRING,
+        dew_point:STRING,
+        gust_kmh:STRING,
+        maximum_air_temperature:STRING,
+        maximum_gust_dir:STRING,
+        maximum_gust_kmh:STRING,
+        maximum_gust_spd:STRING,
+        minimum_air_temperature:STRING,
+        msl_pres:STRING,
+        pres:STRING,
+        qnh_pres:STRING,
+        rainfall:STRING,
+        rainfall_24hr:STRING,
+        rel_humidity:STRING,
+        sea_height:STRING,
+        swell_dir:STRING,
+        swell_height:STRING,
+        swell_period:STRING,
+        vis_km:STRING,
+        weather:STRING,
+        wind_dir:STRING,
+        wind_dir_deg:STRING,
+        wind_gust_spd:STRING,
+        wind_spd:STRING,
+        wind_spd_kmh:STRING>
+) 
+STORED AS PARQUET;
+
